a5ec6d418bfa3bcfbf89337cbeb2a23a341ee857
Juan <lrccfj@outlook.com>
2021-12-01 18:22:47 +0000

    hubspot form ready

--- app.js
+++ app.js
@@ -56,15 +56,7 @@ app.post("/", function (req, res) {
         auth: "eu1-151b-98b3-4364-bd01-66565929d3a5"
     }
     const request = https.request(url, options, function (response) {
-
-        if (response.statusCode === 200) {
-            res.sendFile(__dirname + "/success.html");
-        } else {
-            res.sendFile(__dirname + "/failure.html");
-        }
-        response.on("data", function (data) {
-            console.log(JSON.parse(data));
-        })
+        res.sendFile(__dirname + "/success.html");
     })
 
     request.write(jsonData);
